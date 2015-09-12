e7e366fc43179ad16a1206c6754ef298f7a5b629
alieu93 <adam.lieu@hotmail.com>
2015-09-12 15:20:02 -0400

    Implemented the ability to swipe the screen to scroll through a static number of images.

--- app/src/main/java/testcompany/swipeimagegallery/MainActivity.java
+++ app/src/main/java/testcompany/swipeimagegallery/MainActivity.java
@@ -1,38 +1,63 @@
 package testcompany.swipeimagegallery;
 
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Code for adapter for viewpager
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter();
+        viewPager.setAdapter(adapter);
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    //Class for ViewPager
+    private class ViewPagerAdapter extends PagerAdapter {
+        
+        //Out of memory error when using higher resolution images, app crashes
+        private int[] images = new int[] {
+                R.drawable.image1,
+                R.drawable.image2,
+                R.drawable.image3
+        };
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        @Override
+        public int getCount(){
+            return images.length;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public boolean isViewFromObject(View view, Object object){
+            return view == object;
+        }
+
+        public Object instantiateItem(ViewGroup container, int position){
+            Context context = MainActivity.this;
+            ImageView imageView = new ImageView(context);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            imageView.setImageResource(images[position]);
+            container.addView(imageView, 0);
+            return imageView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object){
+            container.removeView((ImageView) object);
+        }
     }
 }
